06597f31eeec8fe4605e61038700bea1a1b9e5ec
IordachePavel <138784190+IordachePavel@users.noreply.github.com>
2024-07-02 15:00:14 +0300

    Update PDA_MCU_INTERFACE.ino
    The gaming update (Support for joysticks in order to send control via serial, added ability for the PDA to change the baud rate at will, which originally was stuck to 9600 baud.)

--- PDA_MCU_INTERFACE.ino
+++ PDA_MCU_INTERFACE.ino
@@ -20,11 +20,13 @@ ForcedBMP280 climateSensor = ForcedBMP280();
  uint32_t pres_val;    // pressure
 
 ///Information Display Page Manager Variable
-unsigned short int val=3;
+unsigned short int val=3; //This also stores the last known page
 bool reInit=0; /// If user asks to reinitiate a specific page sensor
+bool EnableConstantRefresh= false;
 
 /// Serial Monitor Debug Variables
 bool inUse = false;
+bool EnableNewline = true; //Enables/disables sending \n after a message. Could have fixed this in the RPI program.
 
 ///Button Interface Used Pins 
 const int ButtonA=10;
@@ -33,6 +35,22 @@ const int ButtonC=12;
 const int BCKLGHT=7;
 unsigned short int buttonTimer=100;
 bool backlight=true;
+
+///Joystick Controller Used Pins
+const int LeftPotX = A7;
+const int LeftPotY = A6;
+const int RightPotX = A3;
+const int RightPotY = A2;
+const int LeftButton = A0;
+const int RightButton = A1;
+
+///Enable Controller Constant Display Refresh
+bool GamingMode = false; 
+bool ShowMeasurementMode = false;
+bool ToggledMsrmtMd = false;
+
+///Baud Rate Config
+int BaudAccepted[23]={50, 75, 100, 110, 150, 300, 600, 900, 1200, 1800, 2400, 3600, 4800, 9600, 14400, 19200, 33600, 38400, 56000, 57600, 76800, 115200};
 
 ///Message Interpreter
 String MSG;
@@ -137,6 +155,56 @@ void SerialMonitor(){
   }
 }
 
+void GamingModeController(){
+  int LX = analogRead(LeftPotX);
+  int LY = analogRead(LeftPotY);
+  int RX = analogRead(RightPotX);
+  int RY = analogRead(RightPotY);
+  int cntrlBtn = !digitalRead(LeftButton);
+  int Trig =!digitalRead(RightButton);
+  Serial.print("RX=");
+  Serial.print(RX);
+  Serial.print(", RY=");
+  Serial.print(RY);
+  Serial.print(", LX=");
+  Serial.print(LX);
+  Serial.print(", LY=");
+  Serial.print(LY);
+  Serial.print(", TRIG=");
+  Serial.println(Trig);
+
+    //Toggle joystick debug values
+  if(cntrlBtn){
+    ShowMeasurementMode=!ShowMeasurementMode;
+    if(ShowMeasurementMode)
+    ToggledMsrmtMd= false;
+  }
+
+  if(ShowMeasurementMode){
+  lcd.setCursor(0,0);
+  lcd.println("Gaming mode");
+  lcd.print("X=");
+  lcd.print(RX);
+  lcd.print(", Y=");
+  lcd.print(RY);
+  lcd.println("     ");
+ lcd.print("X=");
+  lcd.print(LX);
+  lcd.print(", Y=");
+  lcd.print(LY);
+  lcd.println("     ");
+  }
+  else{
+    if(!ToggledMsrmtMd){
+      lcd.setCursor(0,0);
+      lcd.println("Gaming mode");
+      lcd.println("Fast Measure");
+      lcd.println("Mode        ");
+      ToggledMsrmtMd = true;
+    }
+  }
+}
+
 /// 1=test,2=done,3=request atm measurements;
 void MessageInterpreter(char comm){
   int valMem=val;
@@ -164,10 +232,15 @@ void MessageInterpreter(char comm){
     Serial.print(pres_val);
     Serial.print(temp_val);
     Serial.print(TempLength);
+    if(EnableNewline)
     Serial.println(PressureLength);
+    else
+    Serial.print(PressureLength);
     break;
     case 2:
     val=3;
+    break;
+    case 5:
     break;
   }
   /// Refresh if value changed from last time. Helps reduce time by not clearing the whole display, as the already existing memory in the display is getting overwritten anyway.
@@ -181,11 +254,14 @@ void pageManager(bool a,bool b, bool c){
   /// Many combinations. Just three buttons. Can also alter global variables.
   unsigned short int memVal=val;
     if(a+b+c){
-  val= 1*a+2*b+4*c;
+  val= 1*a+2*b+4*c;\
+  //if(val){
+ //   LastKnownPage=val;
+ // }
   lcd.clear();
   inUse=0;
     }
-  if(val==7){
+  if(val==7){ ///Reinit sensors
     val=memVal;
     reInit=1;
   }
@@ -208,12 +284,13 @@ void pageManager(bool a,bool b, bool c){
     SerialMonitor();
     break;
     case 4:
-    /// find something else to display here, graphs?
+    /// Gaming Mode.
+    GamingMode=!GamingMode;
     break;
     case 3:
     /// Make a home page and return it here
     lcd.setCursor(0,0);
-    lcd.println("Very manly welcome message placeholder here")
+    lcd.println("Welcome!");
     break;
     
     
@@ -222,34 +299,137 @@ void pageManager(bool a,bool b, bool c){
   delay(100);
 }
 
+void BaudUpdate(String DATA){
+  String ExtractedData;
+  int BaudRate=0;
+    if(cmpNstr(DATA,"BDA",3)){
+      for(int i=3; DATA[i];i++){
+        if(!isdigit(DATA[i])){
+        Serial.println("ERR (Bad specified baud rate format)");
+        return 0;
+        }
+        ExtractedData+=DATA[i];
+      }
+      BaudRate = ExtractedData.toInt();
+      for(int i=0;i<23;i++){
+        if(BaudAccepted[i]==BaudRate){
+          Serial.println("OK!");
+          Serial.end();
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.println("Config OK.");
+          lcd.println("Starting Serial...");
+          Serial.begin(BaudRate);
+          Serial.setTimeout(10);
+          delay(500);
+          return 1;
+        }
+      }
+      Serial.println("ERR (Out of range!)");
+      lcd.setCursor(0,0);
+      lcd.clear();
+      lcd.println("Out of range!");
+      delay(500);
+      return 0;
+    }
+}
+
+bool SerialSetup(){
+  String ExtractedData;
+  int BaudRate=0;
+  lcd.setCursor(0,0);
+  lcd.println("Welcome      ");
+  lcd.println("Please begin ");
+  lcd.println("PDA program.");
+  lcd.println("Baud 9600,");
+  lcd.println("To set:");
+  lcd.println("'BDA(Baud)'");
+  if(Serial.available() > 0){
+    String DATA = Serial.readString();
+    if(cmpNstr(DATA,"BDA",3)){
+      for(int i=3; DATA[i];i++){
+        if(!isdigit(DATA[i])){
+        Serial.println("ERR (Bad specified baud rate format)");
+        return 0;
+        }
+        ExtractedData+=DATA[i];
+      }
+      BaudRate = ExtractedData.toInt();
+      for(int i=0;i<23;i++){
+        if(BaudAccepted[i]==BaudRate){
+          Serial.println("OK!");
+          Serial.end();
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.println("Config OK.");
+          lcd.println("Starting Serial...");
+          Serial.begin(BaudRate);
+          Serial.setTimeout(10);
+          delay(500);
+          return 1;
+        }
+      }
+      Serial.println("ERR (Out of range!)");
+      lcd.setCursor(0,0);
+      lcd.clear();
+      lcd.println("Out of range!");
+      delay(500);
+      return 0;
+    }
+    else{
+      Serial.println("ERR (Bad command)");
+      return 0;
+    }
+  }
+  return 0;
+}
+
+
 void setup(){
   pinMode(ButtonA,INPUT_PULLUP);
   pinMode(ButtonB,INPUT_PULLUP);
   pinMode(ButtonC,INPUT_PULLUP);
   pinMode(BCKLGHT, OUTPUT);
-  Serial.begin(9600);
-  Serial.setTimeout(10);
+  pinMode(RightPotX, INPUT);
+  pinMode(RightPotY, INPUT);
+  pinMode(LeftPotX, INPUT);
+  pinMode(LeftPotY, INPUT);
+  pinMode(LeftButton, INPUT_PULLUP);
+  pinMode(RightButton, INPUT_PULLUP);
+
+
+  
   lcd.begin();
   lcd.setContrast(50);
   lcd.clear(true);
   lcd.clear();
   lcd.setCursor(0, 0);
+  //Serial Config
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  while(!SerialSetup());
   //Start Sensors
   climateSensor.begin();
   initOxSens();
-
   
 }
 
-void loop() {
+void loop() { 
   ///button logic with adjustable time allocated to input
   if(inUse!=1&&val!=2){
    if(Serial.available() > 0){
   MSG=Serial.readString();
   if (cmpNstr(MSG,"MCU",3))
   MessageInterpreter(MSG[4]);
+  else if (cmpNstr(MSG,"BDA",3))
+  BaudUpdate(MSG);
   else{
-    Serial.print(MSG);
+    if(EnableNewline){
+    Serial.println(MSG);
+    }
+    else{
+    Serial.print(MSG);  
+    }
   }
   }   
   }
@@ -258,6 +438,14 @@ void loop() {
     btA=!digitalRead(ButtonA);
     btB=!digitalRead(ButtonB);
     btC=!digitalRead(ButtonC);
+  }
+
+
+  if(val==4){
+    GamingModeController();
+  }
+  else{
+    ToggledMsrmtMd=false;
   }
   /// Page Manager Logic
   pageManager(btA,btB,btC);
