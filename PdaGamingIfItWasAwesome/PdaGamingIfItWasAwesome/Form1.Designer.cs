6afb67160a594553d6b9b908da1e59f3bb005068
IordachePavel <138784190+IordachePavel@users.noreply.github.com>
2024-07-30 14:35:50 +0300

    Add files via upload
    NEW PDA control program II, with adjustable baud rate. Use this one.

---
+++ PdaGamingIfItWasAwesome/PdaGamingIfItWasAwesome/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PdaGamingIfItWasAwesome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BaudBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.JoystickVisualizer = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inputPanel1 = new Microsoft.WindowsCE.Forms.InputPanel(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(320, 213);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(0, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(320, 190);
+            this.tabPage1.Text = "Start";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Initialize";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(7, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 17);
+            this.label3.Text = "To begin, press the \"Initialize\" button.";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.Location = new System.Drawing.Point(7, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 62);
+            this.label2.Text = "Welcome to the \"Joystick remote\" computer program.";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.BaudBox);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(0, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(320, 190);
+            this.tabPage2.Text = "Serial Terminal";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(7, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 62);
+            this.label5.Text = "Received data will be displayed here.";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(264, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 20);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "TX!";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 20);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BaudBox
+            // 
+            this.BaudBox.Items.Add("4800");
+            this.BaudBox.Items.Add("9600");
+            this.BaudBox.Items.Add("19200");
+            this.BaudBox.Items.Add("38400");
+            this.BaudBox.Items.Add("57600");
+            this.BaudBox.Items.Add("112500");
+            this.BaudBox.Location = new System.Drawing.Point(80, 2);
+            this.BaudBox.Name = "BaudBox";
+            this.BaudBox.Size = new System.Drawing.Size(100, 22);
+            this.BaudBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(7, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.Text = "Baud Rate:";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.JoystickVisualizer);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(0, 0);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(320, 190);
+            this.tabPage3.Text = "\"Gaming mode\"";
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // JoystickVisualizer
+            // 
+            this.JoystickVisualizer.BackColor = System.Drawing.Color.Black;
+            this.JoystickVisualizer.Location = new System.Drawing.Point(10, 27);
+            this.JoystickVisualizer.Name = "JoystickVisualizer";
+            this.JoystickVisualizer.Size = new System.Drawing.Size(291, 140);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(10, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 20);
+            this.label6.Text = "TickCounter";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 20);
+            this.label1.Text = "You are currently not gaming.";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label7);
+            this.tabPage4.Location = new System.Drawing.Point(0, 0);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(320, 190);
+            this.tabPage4.Text = "Sensor Results";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(7, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(306, 123);
+            this.label7.Text = "label7";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kyb";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 450;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(320, 240);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Remote";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private Microsoft.WindowsCE.Forms.InputPanel inputPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox BaudBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.IO.Ports.SerialPort SerialPort;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox JoystickVisualizer;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PdaGamingIfItWasAwesome/PdaGamingIfItWasAwesome/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace PdaGamingIfItWasAwesome
+{
+    public partial class Form1 : Form
+    {
+        bool FirstTimeGaming = true;
+        //Set home values:
+        int OrigRX=0;
+        int OrigRY=0;
+        int OrigLX=0;
+        int OrigLY=0;
+
+
+        bool EnableTimerTrigger = false;// I apologize for the solution, but I do not know anymore if it is me, or the outdated software that can't do one thing right, I have used the same thing before in another program and it worked flawlessly, repeated it here and nothing works again. I am at the end of my own power I do not know what to do anymore, I am in a genuine state of despair. "Oh, why didn't you use ChatGPT?", my brother in Christ, not even it can help me solve this problem. Help from who?? I'm all by myself here. Final solution is to keep the timer running from start and enable via if statement the calling of a subroutine. Its gonna take up resources, but at this point, I do not know.
+
+        public int TestTimer = 0;
+        // Toggle keyboard
+        bool isKeyboardEnabled = true;
+
+        // Define the global SerialPort instance
+        private SerialPort PortThatIsSerial;
+
+        // How does the program handle any received serial messages?
+        private int Serial_Reciever_mode=1;
+        //1 Normal: Display any received data to serial port.
+        //2 Gaming: Run the "joystick visualizer".
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize the global SerialPort instance
+            PortThatIsSerial = new SerialPort();
+            PortThatIsSerial.DataReceived += SerialReceived;
+            ActivateSerialPort(9600);
+            tabControl1.TabPages[1].Enabled = false;
+            tabControl1.TabPages[2].Enabled = false;
+            tabControl1.TabPages[3].Enabled = false;
+            timer1.Interval = 1000; // Set interval to 1 second
+            timer1.Tick += new EventHandler( Event_check_gaming);
+            
+            
+        }
+
+
+
+
+        public void MCU_Serial_BaudRateAdjust(int baudrate)
+        {
+            SendSerialMessage("BDA" + Convert.ToString(baudrate));
+            Thread.Sleep(250);
+            DeactivateSerialPort();
+            ActivateSerialPort(baudrate);
+        }
+
+        public void ActivateSerialPort(int ValueBaud)
+        {
+            try
+            {
+                // Begin serial port
+                PortThatIsSerial.PortName = "COM1";
+                PortThatIsSerial.BaudRate = ValueBaud;
+                PortThatIsSerial.Parity = Parity.None;
+                PortThatIsSerial.DataBits = 8;
+                PortThatIsSerial.StopBits = StopBits.One;
+                // Open the serial port
+                PortThatIsSerial.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open the serial port!\nThe program will now close.\nError:" + ex.Message, "Whoops!");
+                PortThatIsSerial.Close();
+                Application.Exit();
+            }
+        }
+
+        public void DeactivateSerialPort()
+        {
+            try
+            {
+                PortThatIsSerial.Close();
+            }
+            catch(Exception ex){ //How would you even get here??
+               MessageBox.Show("Catastrophic failure!\nCannot close the serial port!\nThe program will now close.\nError:" + ex.Message, "HOW?!?!");
+               Application.Exit();
+            }
+        }
+
+        public void SendSerialMessage(string MSG)
+        {
+            try
+            {
+                // Make sure that the serial port is open before writing data
+                if (PortThatIsSerial.IsOpen)
+                {
+                    // Write the data to the serial port
+                    PortThatIsSerial.Write(MSG);
+
+                    // PortThatIsSerial.WriteLine(data);
+                }
+                else
+                {
+                    MessageBox.Show("Somehow, the serial port is not open.\n But I'll reopen it for you!","HOW?!?!");
+                    MessageBox.Show("You really should've not gotten this message, something went wrong.\nIf everything works, carry on, else, restart the program.", "HOW?!?!");
+                    //Start it harder, turn it on and off again.
+                    Exception ExceptionCaught=null;
+                    int BaudSelection = 9600;
+                    if (BaudBox.SelectedIndex != -1)
+                    {
+                        BaudSelection =Convert.ToInt32(BaudBox.Items[BaudBox.SelectedIndex]);
+                    }
+                    try
+                    {
+                        PortThatIsSerial.Close();
+                    }
+                    catch (Exception ex) //It was already closed.
+                    {
+                        ExceptionCaught = ex;
+                        ActivateSerialPort(BaudSelection);
+                    }
+                    if (ExceptionCaught==null)
+                        ActivateSerialPort(BaudSelection);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error transmitting data!\nTry again, if it persists, restart the program. Error: "+ ex.Message);
+            }
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void separateValues(string MSG, out int RX, out int RY, out int LX, out int LY)
+        {
+            RX = RY = LX = LY = 0; 
+            string[] parts = MSG.Split(' '); 
+            foreach (string part in parts)
+            {
+                string[] keyValue = part.Split('=');
+
+                if (keyValue.Length == 2) 
+                {
+                    int value = 0;
+                    try
+                    {
+                        value = Convert.ToInt32(keyValue[1]); 
+                    }
+                    catch (Exception ex)
+                    {
+                        if (FirstTimeGaming)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        //Don't do anything. Nothing happened. its alright. the program must continue.
+                    }
+
+                    if (keyValue[0] == "RX")
+                    {
+                        RX = value;
+                    }
+                    else if (keyValue[0] == "RY")
+                    {
+                        RY = value;
+                    }
+                    else if (keyValue[0] == "LX")
+                    {
+                        LX = value;
+                    }
+                    else if (keyValue[0] == "LY")
+                    {
+                        LY = value;
+                    }
+                }
+            }
+           // MessageBox.Show("Hello I am subroutine and I have successfully ran!!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputPanel1.Enabled = isKeyboardEnabled;
+            isKeyboardEnabled = !isKeyboardEnabled;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int BaudSelection = BaudBox.SelectedIndex;
+            if (BaudSelection != -1)
+            {
+                MCU_Serial_BaudRateAdjust(Convert.ToInt32(BaudBox.Items[BaudSelection] as string));
+            }
+            else
+            {
+                MessageBox.Show("No baud rate selected!\n Serial port is currently set on 9600.", "No selection!");
+            }
+
+        }
+
+        public void Event_check_gaming(object sender, EventArgs e) // I HATE TIMERS!! I HATE TIMERS!!! I HATE TIMERS!!! I HATE TIMERS!! I HATE TIMERS!!! I HATE TIMERS!! I HATE TIMERS!!!
+        {
+            label6.Text = "Tick " + Convert.ToString(TestTimer); // Check if this is still working after the fix.
+            if (EnableTimerTrigger)
+            {
+                TestTimer++;
+                if (Serial_Reciever_mode == 2)
+                {
+                    Serial_Reciever_mode = 1;
+                }
+                else
+                {
+                    EnableTimerTrigger = false;
+                    debugLabel7("Timer has Stopped!");
+
+                    //You are currently not gaming.
+
+                    if (label1.InvokeRequired)
+                    {
+                        label1.BeginInvoke((Action)(() => label1.Text = "You are currently not gaming."));
+                    }
+                    else
+                    {
+                        label1.Text = "You are currently not gaming.";
+                    }
+
+                }
+            }
+        }
+
+        public void GamingJoystickPosition(int LX, int LY, int RX, int RY)
+        {
+            if (FirstTimeGaming)
+            {
+                OrigLX = LX;
+                OrigLY = LY;
+                OrigRX = RX;
+                OrigRY = RY;
+                FirstTimeGaming = false;
+            }
+
+            // ADC max value is 1023.
+            // ADC min value is 0.
+
+           
+        }
+
+
+
+        public void SerialReceived(object sender, SerialDataReceivedEventArgs e)// What to do when a message is received?
+        {
+            string Received = PortThatIsSerial.ReadExisting();
+            if (Received.Contains("LX") && Received.Contains("LY") && Received.Contains("RX") && Received.Contains("RY"))
+            {// its gaming mode measurements, switch to gaming mode;
+                EnableTimerTrigger= true;
+                //debugLabel7("Timer has activated!");
+                Serial_Reciever_mode = 2;
+                
+            }
+
+            if (Serial_Reciever_mode == 2)
+            {
+                int A, B, C, D;
+                separateValues(Received, out A, out B, out C, out D);
+                GamingJoystickPosition(A, B, C, D);
+                //debugLabel7(Convert.ToString(A) + " " + Convert.ToString(B) + " " + Convert.ToString(C) + " " + Convert.ToString(D));
+                if (label1.InvokeRequired)
+                {
+                    label1.BeginInvoke((Action)(() => label1.Text = "You are gaming."));
+                }
+                else
+                {
+                    label1.Text = "You are gaming.";
+                }
+            }
+            
+
+            if (label5.InvokeRequired)
+            {
+                label5.BeginInvoke((Action)(() => label5.Text = Received));
+            }
+            else
+            {
+                label5.Text = Received;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SendSerialMessage(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MCU_Serial_BaudRateAdjust(9600);
+            tabControl1.TabPages[1].Enabled = true;
+            tabControl1.TabPages[2].Enabled = true;
+            tabControl1.TabPages[3].Enabled = true;
+            button1.Enabled = false;
+            label3.Text = "All is OK. You're done with this page.";
+        }
+
+        private void debugLabel7(string MSG)
+        {
+            if (label7.InvokeRequired)
+            {
+                label7.BeginInvoke((Action)(() => label7.Text = MSG));
+            }
+            else
+            {
+                label7.Text = MSG;
+            }
+        }
+
+        private void LY1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar5_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
